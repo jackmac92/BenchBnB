6556a3a800c1313465a4edeb4d54c571f1b2aa18
Jack <jmccown@colgate.edu>
2016-03-25 13:33:29 -0400

    Add Google Maps setup

---
+++ frontend/components/map.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Map = React.createClass({
+
+  render: function() {
+    return (
+      <div className="map" ref="map">
+
+      </div>
+    );
+  }
+
+});
+
+module.exports = Map;
